4b771396839f35743077ec829630e7873032f7a3
michael ng <mngksoon@gmail.com>
2019-11-27 16:17:07 +0800

    added seed, homepage and pet page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,32 @@ Pet.destroy_all if Rails.env.development? || Rails.env.test?
 
 puts "Creating the seeds"
 Pet.create!(
-  name: "Lemonade"
+  name: "Lemonade",
+  image_url: "https://source.unsplash.com/featured/?dog",
+  description: "best friend ever",
+  category: "dog",
+  age: 3,
+  gender: "male",
+  HDB_approved: true,
+  owner: Owner.first
 )
 Pet.create!(
-  name: "Spritzer"
+  name: "Spritzer",
+  image_url: "https://source.unsplash.com/featured/?dog",
+  description: "best best friend ever",
+  category: "dog",
+  age: 4,
+  gender: "male",
+  HDB_approved: true,
+  owner: Owner.first
 )
 Pet.create!(
-  name: "Minty"
+  name: "Minty",
+  image_url: "https://source.unsplash.com/featured/?dog",
+  description: "best best best friend ever",
+  category: "dog",
+  age: 5,
+  gender: "male",
+  HDB_approved: true,
+  owner: Owner.first
 )
